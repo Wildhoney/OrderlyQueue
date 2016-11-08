49ec07adc047eca15d13b248cae4dcde8fc54298
Wildhoney <adam.timberlake@gmail.com>
2016-11-08 22:24:13 +0000

    Added pseudo-observable

--- src/OrderlyQueue.js
+++ src/OrderlyQueue.js
@@ -1,33 +1,58 @@
-// const delay = () => {
-//
-//     return new Promise(resolve => {
-//
-//         setTimeout(() => resolve(Math.random()), 2000);
-//
-// });
-
-// };
-
 /**
- * @constant initialTask
- * @type {Function}
+ * @method fn
+ * @return {void}
  */
-const initialTask = () => Promise.resolve();
+const fn = Function.prototype;
 
 /**
+ * @param {Object} [value]
+ * @param {Function} [next]
+ * @param {Function} [error]
  * @return {Object}
  */
-export default function() {
+export default function({ value, next = fn, error = fn }) {
+
+    /**
+     * @method initialTask
+     * @return {Promise}
+     */
+    const initialTask = () => Promise.resolve(value);
 
     /**
      * @method enqueue
      * @param {Function} createTask
+     * @param {Object} [props]
      * @yield {*}
      */
-    async function* enqueue (createTask = initialTask) {
-        yield* enqueue(yield await createTask());
+    async function* enqueue(createTask = initialTask, props) {
+
+        try {
+
+            // Invoke the next task, passing in either the default props, or the props from the
+            // previous task.
+            const task = await createTask(props);
+
+            // Publish the result of the task to the subscription.
+            next(task);
+
+            // Recursively invoke the generator, passing in the props received from the last
+            // invocation.
+            yield* enqueue(yield, task);
+
+        } catch (reason) {
+
+            // Task rejected the promise with a potential reason, which we'll publish to the
+            // subscription.
+            error(reason);
+
+            // Any further invocations will simply continue from the last success.
+            yield* enqueue(yield, props);
+
+        }
+
     }
 
+    // Initiate the async generator, and move the cursor to the first yield.
     const iterator = enqueue();
     iterator.next();
 
