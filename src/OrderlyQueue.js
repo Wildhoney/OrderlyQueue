154fb0510d0d3e538d5d1027e3226344d5c2ba42
Wildhoney <adam.timberlake@gmail.com>
2016-11-09 18:16:41 +0000

    Propagating rejected reason to 'catch', and changed fn 'add' to 'process'

--- src/OrderlyQueue.js
+++ src/OrderlyQueue.js
@@ -13,58 +13,60 @@ const fn = Function.prototype;
 export default function({ value, next = fn, error = fn }) {
 
     /**
+     * Initial task the yields the value that was passed in upon instantiation.
+     *
      * @method initialTask
      * @return {Promise}
      */
     const initialTask = () => Promise.resolve(value);
 
     /**
-     * @method enqueue
+     * @method queue
      * @param {Function} createTask
      * @param {Object} [props]
-     * @yield {*}
+     * @yield {void}
      */
-    async function* enqueue(createTask = initialTask, props) {
+    async function* queue(createTask = initialTask, props) {
 
         try {
 
             // Invoke the next task, passing in either the default props, or the props from the
-            // previous task.
+            // previous task, and awaits for its promise to be resolved.
             const task = await createTask(props);
 
-            // Publish the result of the task to the subscription.
+            // Publish the resolved value of the task to the subscription.
             next(task);
 
             // Recursively invoke the generator, passing in the props received from the last
             // invocation.
-            yield* enqueue(yield, task);
+            yield* queue(yield task, task);
 
         } catch (reason) {
 
-            // Task rejected the promise with a potential reason, which we'll publish to the
-            // subscription.
+            // Publish the rejected value of the task to the subscription.
             error(reason);
 
-            // Any further invocations will simply continue from the last success.
-            yield* enqueue(yield, props);
+            // Any further invocations will simply continue from the last successful task, which
+            // allows recovery from any promise rejections.
+            yield* queue(yield reason, props);
 
         }
 
     }
 
     // Initiate the async generator, and move the cursor to the first yield.
-    const iterator = enqueue();
+    const iterator = queue();
     iterator.next();
 
     /**
-     * @method add
+     * @method process
      * @param {Function} promiseFn
-     * @return {void}
+     * @return {Promise}
      */
-    const add = promiseFn => {
-        iterator.next(promiseFn);
+    const process = promiseFn => {
+        return iterator.next(promiseFn);
     };
 
-    return { add, iterator };
+    return { process, iterator };
 
 }
