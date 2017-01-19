0cd4ff6f0d87be35a537169c582a5a649e9d39fe
Wildhoney <adam.timberlake@gmail.com>
2017-01-19 22:27:58 +0000

    Removed Babel in favour of pure Rollup with 'async' plugin

--- dist/orderly-queue.js
+++ dist/orderly-queue.js
@@ -29,21 +29,21 @@ function __asyncGen(g) {
  * @method fn
  * @return {void}
  */
-var fn = Function.prototype;
+const fn = Function.prototype;
 
 /**
  * @method message
  * @param {String} message
  */
-var message = function message(_message) {
-  return 'OrderlyQueue: ' + _message + '.';
+const message = message => {
+  return `OrderlyQueue: ${ message }.`;
 };
 
 /**
  * @constant defaultOptions
  * @type {Object}
  */
-var defaultOptions = { value: null, next: fn, error: fn };
+const defaultOptions = { value: null, next: fn, error: fn };
 
 /**
  * @param {Object} [value = null]
@@ -51,14 +51,7 @@ var defaultOptions = { value: null, next: fn, error: fn };
  * @param {Function} [error = Function.prototype]
  * @return {Object}
  */
-var orderlyQueue = function () {
-  var _ref = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : defaultOptions,
-      _ref$value = _ref.value,
-      value = _ref$value === undefined ? null : _ref$value,
-      _ref$next = _ref.next,
-      next = _ref$next === undefined ? fn : _ref$next,
-      _ref$error = _ref.error,
-      error = _ref$error === undefined ? fn : _ref$error;
+var orderlyQueue = function ({ value = null, next = fn, error = fn } = defaultOptions) {
 
   /**
    * Initial task the yields the value that was passed in upon instantiation.
@@ -66,9 +59,7 @@ var orderlyQueue = function () {
    * @method initialTask
    * @return {Promise}
    */
-  var initialTask = function initialTask() {
-    return Promise.resolve(value);
-  };
+  const initialTask = () => Promise.resolve(value);
 
   /**
    * @method queue
@@ -77,68 +68,34 @@ var orderlyQueue = function () {
    * @yield {void}
    */
   function queue(createTask, props) {
-    return __asyncGen(regeneratorRuntime.mark(function _callee() {
-      var result;
-      return regeneratorRuntime.wrap(function _callee$(_context) {
-        while (1) {
-          switch (_context.prev = _context.next) {
-            case 0:
-              _context.prev = 0;
-              _context.next = 3;
-              return { __await: createTask(props) };
+    return __asyncGen(function* () {
 
-            case 3:
-              result = _context.sent;
+      try {
 
+        // Invoke the next task, passing in either the default props, or the props from the
+        // previous task, and awaits for its promise to be resolved.
+        const result = yield { __await: createTask(props) };
 
-              // Publish the resolved value of the task to the subscription.
-              next(result);
+        // Publish the resolved value of the task to the subscription.
+        next(result);
 
-              // Recursively invoke the generator, passing in the props received from the last
-              // invocation.
-              _context.t0 = queue;
-              _context.next = 8;
-              return result;
+        // Recursively invoke the generator, passing in the props received from the last
+        // invocation.
+        yield* queue((yield result), result);
+      } catch (reason) {
 
-            case 8:
-              _context.t1 = _context.sent;
-              _context.t2 = result;
-              return _context.delegateYield((0, _context.t0)(_context.t1, _context.t2), 't3', 11);
+        // Publish the rejected value of the task to the subscription.
+        error(reason);
 
-            case 11:
-              _context.next = 22;
-              break;
-
-            case 13:
-              _context.prev = 13;
-              _context.t4 = _context['catch'](0);
-
-
-              // Publish the rejected value of the task to the subscription.
-              error(_context.t4);
-
-              // Any further invocations will simply continue from the last successful task, which
-              // allows recovery from any promise rejections.
-              _context.t5 = queue;
-              _context.next = 19;
-              return _context.t4;
-
-            case 19:
-              _context.t6 = _context.sent;
-              _context.t7 = props;
-              return _context.delegateYield((0, _context.t5)(_context.t6, _context.t7), 't8', 22);
-
-            case 22:
-            case 'end':
-              return _context.stop();
-          }
-        }
-      }, _callee, this, [[0, 13]]);
-    })());
+        // Any further invocations will simply continue from the last successful task, which
+        // allows recovery from any promise rejections.
+        yield* queue((yield reason), props);
+      }
+    }());
   }
 
   // Initiate the async generator, and move the cursor to the first yield.
-  var iterator = queue(initialTask);
+  const iterator = queue(initialTask);
   iterator.next();
 
   /**
@@ -146,7 +103,7 @@ var orderlyQueue = function () {
    * @param {Function} promiseFn
    * @return {Promise}
    */
-  var process = function process(promiseFn) {
+  const process = promiseFn => {
 
     if (typeof promiseFn !== 'function') {
       throw new Error(message('Passed in task to `queue` must be a function that yields a promise'));
@@ -155,9 +112,7 @@ var orderlyQueue = function () {
     return iterator.next(promiseFn);
   };
 
-  return { process: process, abort: function abort() {
-      return iterator.return();
-    } };
+  return { process, abort: () => iterator.return() };
 };
 
 module.exports = orderlyQueue;
